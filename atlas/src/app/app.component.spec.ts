7230a0d285e4f0006608f0dd174e2ff1fb1ec775
Natalia Gurgul <natalia.gurgul@gmail.com>
2023-02-15 16:56:01 +0100

    Creating footer

--- atlas/src/app/app.component.spec.ts
+++ atlas/src/app/app.component.spec.ts
@@ -1,14 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/footer/footer.component';
 import { HeaderComponent } from './core/header/header.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, MatToolbarModule],
-      declarations: [AppComponent, HeaderComponent],
+      imports: [RouterTestingModule, MatToolbarModule, MatButtonModule],
+      declarations: [AppComponent, HeaderComponent, FooterComponent],
     }).compileComponents();
   });
 
--- atlas/src/app/core/core.module.ts
+++ atlas/src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './footer/footer.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [CommonModule, MatToolbarModule],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
