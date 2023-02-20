9fc017aba7be6e939fa4207b822a192632f9fc6a
Natalia Gurgul <natalia.gurgul@gmail.com>
2023-02-20 12:06:34 +0100

    Adding unit tests for header

--- atlas/src/app/core/header/header.component.spec.ts
+++ atlas/src/app/core/header/header.component.spec.ts
@@ -25,14 +25,43 @@ describe('HeaderComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should have h1', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('h1')).toBeDefined;
+  });
+
   it('should render logo', () => {
     const fixture = TestBed.createComponent(HeaderComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
+    
     expect(
       compiled.querySelector('img.app-logo')?.getAttribute('src')
     )?.toContain(
       '/assets/logo/atlas-low-resolution-logo-black-on-white-background-cropped-min.svg'
     );
+  });
+
+  it('should render back button', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    component.backBtnShown = true;
+    fixture.detectChanges();
+
+    expect(compiled.querySelector('button.prev-page-btn')).toBeDefined;
+  });
+
+  it('should not render back button', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    component.backBtnShown = false;
+    fixture.detectChanges();
+
+    expect(compiled.querySelector('button.prev-page-btn')).not.toBeDefined;
   });
 });
